7604f66d102ee323de682401ccbcf57c413377fe
Brahm <brahm.kelly@gmail.com>
2022-05-13 11:33:46 -0400

    code good,need graphics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+ const checkCode = (display) => {
+   if (display ==='4112'){
+    setDisplay('ENTER')
+   } else { setDisplay('INVALID')}
+ }
+  const [display, setDisplay] = useState(' ')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+      
+      <main>
+      <h3 class="display">{display}</h3>
+      <button onClick={()=>setDisplay(display +'1')}>1</button>
+      <button onClick={()=>setDisplay(display +'2')}>2</button>
+      <button onClick={()=>setDisplay(display +'3')}>3</button>
+      <br></br>
+      <button onClick={()=>setDisplay(display +'4')}>4</button>
+      <button onClick={()=>setDisplay(display +'5')}>5</button>
+      <button onClick={()=>setDisplay(display +'6')}>6</button>
+      <br></br>
+      <button onClick={()=>setDisplay(display +'7')}>7</button>
+      <button onClick={()=>setDisplay(display +'8')}>8</button>
+      <button onClick={()=>setDisplay(display +'9')}>9</button>
+      <br></br>
+      <button onClick={()=>setDisplay('')}>C</button>
+      <button onClick={()=>setDisplay(display +'0')}>0</button>
+      <button onClick={checkCode}>E</button>
+      </main>
+      
     </div>
+    
   );
 }
 
